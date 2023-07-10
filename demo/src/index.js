81f00530f1bc2ccd81e1410ac62533539272a146
Satoru <you@example.com>
2023-07-10 18:43:27 +0200

    feat: minor update

--- demo/src/index.js
+++ demo/src/index.js
@@ -11,6 +11,7 @@ const config = {
       // loadedManifest: 'https://www.hi.u-tokyo.ac.jp/collection/digitalgallery/ryukyu/data/iiif/0001/manifest.json',
       loadedManifest: 'http://127.0.0.1:5501/local/manifest2.json',
     },
+
     {
       // loadedManifest: 'https://www.hi.u-tokyo.ac.jp/collection/digitalgallery/ryukyu/data/iiif/0001/manifest.json',
       loadedManifest: 'http://127.0.0.1:5501/local/0001.json',
@@ -23,6 +24,8 @@ const config = {
       // loadedManifest: 'https://www.hi.u-tokyo.ac.jp/collection/digitalgallery/ryukyu/data/iiif/0001/manifest.json',
       loadedManifest: 'http://127.0.0.1:5501/local/0003.json',
     },
+    /*
+    */
   ],
 };
 
