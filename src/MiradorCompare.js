20eafc0b2b547cf82aeb19478719d01ad522f2bc
Satoru <you@example.com>
2023-07-12 09:39:05 +0200

    feat: fix bug

--- src/MiradorCompare.js
+++ src/MiradorCompare.js
@@ -1,4 +1,4 @@
-import React, { Component /* , useState , useEffect */ } from 'react';
+import React, { Component } from 'react';
 // import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import MiradorMenuButton from 'mirador/dist/es/src/containers/MiradorMenuButton';
@@ -10,17 +10,12 @@ import Typography from '@material-ui/core/Typography';
 import ScrollIndicatedDialogContent from 'mirador/dist/es/src/containers/ScrollIndicatedDialogContent';
 import mirador from 'mirador';
 import CommentIcon from '@material-ui/icons/Comment';
-import { getContainerId } from 'mirador/dist/es/src/state/selectors/config';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuItem from '@material-ui/core/MenuItem';
 
 import Draggable from 'react-draggable';
 import Paper from '@material-ui/core/Paper';
@@ -32,17 +27,14 @@ const compareReducer = (state = {}, action) => {
   if (action.type === 'OPEN_WINDOW_DIALOG') {
     return {
       ...state,
-      // windowListAnchor: action.target,
       isDialogOpen: 1,
     };
   }
 
-  /*
-
   if (action.type === 'CLOSE_WINDOW_DIALOG') {
     return {
       ...state,
-      windowListAnchor: false,
+      isDialogOpen: 0,
     };
   }
 
@@ -59,7 +51,6 @@ const compareReducer = (state = {}, action) => {
       xy: action.xy,
     };
   }
-  */
 
   if (action.type === 'SET_COMPARE') {
     return {
@@ -89,15 +80,11 @@ class CompareComponent extends Component {
     const { scrollTop } = element;
 
     setScrollTop(scrollTop);
-
     closeDialog();
   }
 
-  handleOpen(event) {
+  handleOpen() {
     const { openDialog } = this.props;
-
-    // const currentTarget = event ? event.currentTarget : null;
-    // openDialog(/* currentTarget */1);
     openDialog();
   }
 
@@ -125,47 +112,24 @@ class CompareComponent extends Component {
   render() {
     console.log('⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️');
 
-    // const { state /* , classes */ } = this.props;
-
     const state = this.props;
-    console.log('state', state);
 
-    // const stateString = JSON.stringify(state, null, 2);
-    // console.log('stateString', stateString.length);
-    // console.log('stateString', stateString);
+    const {
+      xy, isDialogOpen, scrollTop, rows,
+    } = state;
 
-    // const containerId = getContainerId(state);
-    // console.log('containerId', containerId);
-
-    // const windowListAnchor = state.compareReducer ? state.compareReducer.windowListAnchor : null;
-    // console.log({ windowListAnchor });
-
-    /*
-    console.log('state', state);
-    // const disabled = false;
-    // const windowCount = 0;
-
-    const xy = state.compareReducer && state.compareReducer.xy
-      ? state.compareReducer.xy : { x: 0, y: 0 };
-
-    const rows = getRows(state); // []; // getRows(state);
+    const rowsObj = JSON.parse(rows);
 
     // eslint-disable-next-line no-undef
     window.setTimeout(() => {
       const scrollComponent = document.getElementById('scrollComponent');
       if (scrollComponent) {
-        scrollComponent.scrollTop = state.compareReducer ? state.compareReducer.scrollTop : 0;
+        scrollComponent.scrollTop = scrollTop;
       }
     }, 0.1);
 
-    console.log('render');
-    */
-
-    //     onClick={(e) => this.handleOpen(e)}
-
     return (
       <>
-
         <MiradorMenuButton
           aria-haspopup="true"
           aria-label="List All Annotations"
@@ -173,40 +137,97 @@ class CompareComponent extends Component {
           onClick={(e) => this.handleOpen(e)}
         >
           <CommentIcon />
-
         </MiradorMenuButton>
 
-        {/*
-          container={document.querySelector(`#${containerId} .mirador-viewer`)}
+        <Dialog
+          disableEnforceFocus
+          scroll="paper"
+          fullWidth
+          maxWidth="xs"
+          open={isDialogOpen}
+          style={{ height: '60vh' }}
+          BackdropProps={{ style: { backgroundColor: 'transparent' } }}
+          onClose={(event, reason) => {
+            if (reason === 'backdropClick') {
+              this.handleClose();
+            }
+          }}
+          PaperComponent={PaperComponent}
+          PaperProps={{ x: xy.x, y: xy.y, handleStop: this.handleStop }}
+        >
+          <>
+            <DialogTitle style={{ cursor: 'move' }} disableTypography id="draggable-dialog-title">
+              <Typography variant="h2">Annotations</Typography>
+            </DialogTitle>
+            <ScrollIndicatedDialogContent id="scrollComponent">
 
-        <div>
-          aaa
-        </div>
-        */}
+              <Table size="small" aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Size</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rowsObj.map((row) => (
+                    <TableRow
+                      hover
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => this.compareItem(row.windows)}
+                      key={row.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.id}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {row.labels.join(', ')}
+                      </TableCell>
+                      <TableCell align="right">{row.windows.length}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </ScrollIndicatedDialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={() => this.handleClose()}>
+                Close
+              </Button>
+            </DialogActions>
+          </>
+        </Dialog>
       </>
     );
   }
 }
 
 CompareComponent.propTypes = {
+  xy: PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+  }),
+  isDialogOpen: PropTypes.number,
+  scrollTop: PropTypes.number,
   setScrollTop: PropTypes.func,
   compare: PropTypes.func,
   openDialog: PropTypes.func,
   closeDialog: PropTypes.func,
-  // classes: PropTypes.objectOf(PropTypes.string).isRequired,
-  // t: PropTypes.func,
-  // eslint-disable-next-line react/forbid-prop-types
-  // state: PropTypes.any.isRequired,
   setXY: PropTypes.func,
+  // eslint-disable-next-line react/forbid-prop-types
+  rows: PropTypes.arrayOf(PropTypes.any),
 };
 
 CompareComponent.defaultProps = {
+  xy: { x: 0, y: 0 },
+  scrollTop: 0,
+  isDialogOpen: 0,
   setScrollTop: () => { },
   compare: () => { },
   openDialog: () => { },
   closeDialog: () => { },
   setXY: () => { },
-  // t: () => {},
+  rows: [],
 };
 
 const compareAction = (windowId, boxToZoom) => (dispatch) => {
@@ -215,28 +236,23 @@ const compareAction = (windowId, boxToZoom) => (dispatch) => {
     y: boxToZoom.y + boxToZoom.height / 2,
   };
 
-  // console.log('zoomCenter', zoomCenter);
-
   dispatch(mirador.actions.updateViewport(windowId, {
     x: zoomCenter.x,
     y: zoomCenter.y,
     zoom: 1 / boxToZoom.width,
   }));
-
-  /*
-  */
 };
 
 const mapStateToProps = (state) => ({
-  // state,
-  aaa: getContainerId(state),
-  // windowListAnchor: state.compareReducer ? state.compareReducer.windowListAnchor : null,
-  isDialogOpen: state.compareReducer ? state.compareReducer.isDialogOpen : null,
+  rows: JSON.stringify(getRows(state)),
+  scrollTop: state.compareReducer ? state.compareReducer.scrollTop : 0,
+  xy: state.compareReducer ? state.compareReducer.xy : { x: 0, y: 0 },
+  isDialogOpen: state.compareReducer ? state.compareReducer.isDialogOpen : 0,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   compare: (windowId, boxToZoom) => dispatch(compareAction(windowId, boxToZoom)),
-  openDialog: () => dispatch({ type: 'OPEN_WINDOW_DIALOG' /* , target: currentTarget */ }), // currentTarget
+  openDialog: () => dispatch({ type: 'OPEN_WINDOW_DIALOG' }),
   closeDialog: () => dispatch({ type: 'CLOSE_WINDOW_DIALOG' }),
   setScrollTop: (scrollTop) => dispatch({ type: 'SET_SCROLL_TOP', scrollTop }),
   setXY: (xy) => dispatch({ type: 'SET_XY', xy }),
@@ -244,7 +260,6 @@ const mapDispatchToProps = (dispatch) => ({
 
 export default {
   target: 'WorkspaceControlPanelButtons',
-  // target: 'WindowTopBarPluginMenu',
   mode: 'add',
   component: CompareComponent,
   mapDispatchToProps,
@@ -253,84 +268,3 @@ export default {
     compareReducer,
   },
 };
-
-/*
-
-      className={
-              classNames(classes.ctrlBtn, (open ? classes.ctrlBtnSelected : null))
-            }
-      */
-
-/*
-      const open = false; // true;
-
-      const classes = {
-        ctrBtn: 'WithPlugins(WorkspaceControlPanelButtons)-ctrlBtn-12',
-      };
-      */
-
-// .join(', ')
-
-/*
-{Boolean(windowListAnchor) && (
-          <Dialog
-            disableEnforceFocus
-            scroll="paper"
-            fullWidth
-            maxWidth="xs"
-            open
-            style={{ height: '60vh' }}
-            BackdropProps={{ style: { backgroundColor: 'transparent' } }}
-            onClose={(event, reason) => {
-              if (reason === 'backdropClick') {
-                this.handleClose();
-              }
-            }}
-            PaperComponent={PaperComponent}
-            PaperProps={{ x: xy.x, y: xy.y, handleStop: this.handleStop }}
-          >
-            <>
-              <DialogTitle style={{ cursor: 'move' }} disableTypography id="draggable-dialog-title">
-                <Typography variant="h2">Annotations</Typography>
-              </DialogTitle>
-              <ScrollIndicatedDialogContent id="scrollComponent">
-
-                <Table size="small" aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>ID</TableCell>
-                      <TableCell>Name</TableCell>
-                      <TableCell align="right">Size</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row) => (
-                      <TableRow
-                        hover
-                        style={{ cursor: 'pointer' }}
-                        onClick={() => this.compareItem(row.windows)}
-                        key={row.id}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-                        <TableCell component="th" scope="row">
-                          {row.id}
-                        </TableCell>
-                        <TableCell component="th" scope="row">
-                          {row.labels.join(', ')}
-                        </TableCell>
-                        <TableCell align="right">{row.windows.length}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </ScrollIndicatedDialogContent>
-              <DialogActions>
-                <Button color="primary" onClick={() => this.handleClose()}>
-                  Close
-                </Button>
-              </DialogActions>
-            </>
-
-          </Dialog>
-        )}
-        */
